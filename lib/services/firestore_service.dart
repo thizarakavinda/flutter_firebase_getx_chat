a3b87ac7ec03295f2a7e27f8d79aae1d6b7afea9
Thisara Kavinda <thizarakavinda@gmail.com>
2026-04-18 22:27:30 +0530

    firebase auth service init

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -23,4 +23,28 @@ class FirestoreService {
       throw Exception('Failed to Get User: ${e.toString()}');
     }
   }
+
+  Future<void> updateUserOnlineStatus(String userId, bool isOnline) async {
+    try {
+      DocumentSnapshot doc = await _firestore.collection('users').doc(userId).get();
+      if (doc.exists) {
+        await _firestore.collection('users').doc(userId).update(
+          {
+            'isOnline': isOnline,
+            'lastSeen': DateTime.now().microsecondsSinceEpoch,
+          },
+        );
+      }
+    } catch (e) {
+      throw Exception('Failed to Update Online Status: ${e.toString()}');
+    }
+  }
+
+  Future<void> deleteUser(String userId) async {
+    try {
+      await _firestore.collection('users').doc(userId).delete();
+    } catch (e) {
+      throw Exception('Failed to Delete User: ${e.toString()}');
+    }
+  }
 }
