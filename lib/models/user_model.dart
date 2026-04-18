73ab6979223ec445811e73281156df93a5dbfdff
Thisara Kavinda <thizarakavinda@gmail.com>
2026-04-18 21:56:15 +0530

    user model class created

---
+++ lib/models/user_model.dart
@@ -0,0 +1,63 @@
+class UserModel {
+  final String id;
+  final String email;
+  final String displayName;
+  final String? photoUrl;
+  final bool isOnline;
+  final DateTime lastSeen;
+  final DateTime createdAt;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.displayName,
+    this.photoUrl = "",
+    this.isOnline = false,
+    required this.lastSeen,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'email': email,
+      'displayName': displayName,
+      'photoUrl': photoUrl,
+      'isOnline': isOnline,
+      'lastSeen': lastSeen,
+      'createdAt': createdAt,
+    };
+  }
+
+  static UserModel fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] ?? "",
+      email: map['email'] ?? "",
+      displayName: map['displayName'] ?? "",
+      photoUrl: map['photoUrl'] ?? "",
+      isOnline: map['isOnline'] ?? false,
+      lastSeen:  DateTime.fromMillisecondsSinceEpoch( map['lastSeen']?.millisecondsSinceEpoch ?? 0),
+      createdAt: DateTime.fromMillisecondsSinceEpoch( map['createdAt']?.millisecondsSinceEpoch ?? 0),
+    );
+  }
+
+  UserModel copyWith({
+    String? id,
+    String? email,
+    String? displayName,
+    String? photoUrl,
+    bool? isOnline,
+    DateTime? lastSeen,
+    DateTime? createdAt,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      displayName: displayName ?? this.displayName,
+      photoUrl: photoUrl ?? this.photoUrl,
+      isOnline: isOnline ?? this.isOnline,
+      lastSeen: lastSeen ?? this.lastSeen,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,13 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
+
+// class AuthService {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final FirestoreService _firebaseService = FirestoreService();
+
+//   User? get currentUser => _auth.currentUser;
+//   String? get currentUserId => _auth.currentUser?.uid;
+
+//   Stream<User?> get authStateChange => _auth.authStateChanges();
+// }
