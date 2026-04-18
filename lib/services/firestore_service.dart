6e6d5bdff097b17e3e3d31a89a34465c546e8447
Thisara Kavinda <thizarakavinda@gmail.com>
2026-04-18 22:22:07 +0530

    firestore services init

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,1 +1,26 @@
-class FirestoreService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user_model.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> createUser(UserModel user) async {
+    try {
+      await _firestore.collection('users').doc(user.id).set(user.toMap());
+    } catch (e) {
+      throw Exception('Failed to Create User: ${e.toString()}');
+    }
+  }
+
+  Future<UserModel?> getUserById(String userId) async {
+    try {
+      DocumentSnapshot doc = await _firestore.collection('users').doc(userId).get();
+      if (doc.exists) {
+        return UserModel.fromMap(doc.data() as Map<String, dynamic>);
+      }
+      return null;
+    } catch (e) {
+      throw Exception('Failed to Get User: ${e.toString()}');
+    }
+  }
+}
