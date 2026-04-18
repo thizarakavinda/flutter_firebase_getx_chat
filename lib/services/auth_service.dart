de257f16310ab4e0f0416a3142392304a57aca85
Thisara Kavinda <thizarakavinda@gmail.com>
2026-04-18 22:16:28 +0530

    firebase auth logics implement

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,13 +1,97 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase_getx_chat/models/user_model.dart';
 
-// class AuthService {
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//   final FirestoreService _firebaseService = FirestoreService();
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirestoreService _firestoreService = FirestoreService();
 
-//   User? get currentUser => _auth.currentUser;
-//   String? get currentUserId => _auth.currentUser?.uid;
+  User? get currentUser => _auth.currentUser;
+  String? get currentUserId => _auth.currentUser?.uid;
 
-//   Stream<User?> get authStateChange => _auth.authStateChanges();
-// }
+  Stream<User?> get authStateChange => _auth.authStateChanges();
+
+  Future<UserModel?> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = result.user;
+      if (user != null) {
+        await _firestoreService.updateUserOnlineStatus(user.uid, true);
+        return await _firestoreService.getUserById(user.uid);
+      }
+      return null;
+    } catch (e) {
+      throw Exception('Failed to sign in: ${e.toString()}');
+    }
+  }
+
+  Future<UserModel?> registerWithEmailAndPassword(
+    String email,
+    String password,
+    String displayName,
+  ) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = result.user;
+      if (user != null) {
+        await user.updateDisplayName(displayName);
+        final userModel = UserModel(
+          id: user.uid,
+          email: email,
+          displayName: displayName,
+          photoUrl: "",
+          isOnline: true,
+          lastSeen: DateTime.now(),
+          createdAt: DateTime.now(),
+        );
+        await _firestoreService.createUser(userModel);
+        return userModel;
+      }
+      return null;
+    } catch (e) {
+      throw Exception('Failed to sign in: ${e.toString()}');
+    }
+  }
+
+  Future<void> sendPasswordResetEmail(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      throw Exception('Failed to send password reset email: ${e.toString()}');
+    }
+  } 
+
+  Future<void> signOut() async {
+    try {
+      if (currentUser != null) {
+        await _firestoreService.updateUserOnlineStatus(currentUserId!, false);
+      }
+      await _auth.signOut();
+    } catch (e) {
+      throw Exception('Failed to sign out: ${e.toString()}');
+    }
+  }
+
+  Future<void> deleteAccount() async {
+    try {
+      User? user = currentUser;
+      if (user != null) {
+        await _firestoreService.deleteUser(user.uid);
+        await user.delete();
+      }
+      await _auth.currentUser?.delete();
+    } catch (e) {
+      throw Exception('Failed to Delete Account: ${e.toString()}');
+    }
+  }
+}
