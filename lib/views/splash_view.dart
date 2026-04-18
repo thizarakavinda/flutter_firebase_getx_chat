e1c8efadd4bfeac59b71aa6c265886d7768b6cb6
Thisara Kavinda <thizarakavinda@gmail.com>
2026-04-18 21:06:07 +0530

    define splash view animations

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -7,7 +7,33 @@ class SplashView extends StatefulWidget {
   State<SplashView> createState() => _SplashViewState();
 }
 
-class _SplashViewState extends State<SplashView> {
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.elasticOut),
+    );
+
+    _animationController.forward();
+
+    // _checkAuthAndNavigate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
