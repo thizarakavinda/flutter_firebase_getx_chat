0676d009ffa61961b9f028b116f22ad6cbdb7b14
Thisara Kavinda <thizarakavinda@gmail.com>
2026-04-25 14:41:22 +0530

    create message model

---
+++ lib/models/message_model.dart
@@ -0,0 +1,40 @@
+enum MessageType { text }
+
+class MessageModel {
+  final String id;
+  final String senderId;
+  final String receiverId;
+  final String content;
+  final MessageType type;
+  final DateTime timestamp;
+  final bool isRead;
+  final bool isEdited;
+  final DateTime? editedAt;
+
+  MessageModel({
+    required this.id,
+    required this.senderId,
+    required this.receiverId,
+    required this.content,
+    this.type = MessageType.text,
+    required this.timestamp,
+    this.isRead = false,
+    this.isEdited = false,
+    this.editedAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'senderId': senderId,
+      'receiverId': receiverId,
+      'content': content,
+      'type': type.name,
+      'timestamp': timestamp.millisecondsSinceEpoch,
+      'isRead': isRead,
+      'isEdited': isEdited,
+      'editedAt': editedAt?.millisecondsSinceEpoch,
+    };
+  }
+}
+
