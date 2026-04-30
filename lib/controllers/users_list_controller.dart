327cd779c227cc784b22d59cec972a7c2c0465fc
Thisara Kavinda <thizarakavinda@gmail.com>
2026-04-30 23:12:04 +0530

    users list controller init

---
+++ lib/controllers/users_list_controller.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_firebase_getx_chat/controllers/auth_controller.dart';
+import 'package:flutter_firebase_getx_chat/models/friend_request_model.dart';
+import 'package:flutter_firebase_getx_chat/models/friendship_model.dart';
+import 'package:flutter_firebase_getx_chat/models/user_model.dart';
+import 'package:flutter_firebase_getx_chat/services/firestore_service.dart';
+import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
+
+enum UserRelationshipStatus { friendRequestReceived, friends, blocked }
+
+class UsersListController extends GetxController {
+  final FirestoreService _firestoreService = FirestoreService();
+  final AuthController _authController = Get.find<AuthController>();
+  final Uuid _uuid = Uuid();
+
+  final RxList<UserModel> _users = <UserModel>[].obs;
+  final RxList<UserModel> _filteredUsers = <UserModel>[].obs;
+  final RxBool _isLoading = false.obs;
+  final RxString _searchQuery = ''.obs;
+  final RxString _error = ''.obs;
+
+  final RxMap<String, UserRelationshipStatus> _userRelationships =
+      <String, UserRelationshipStatus>{}.obs;
+  final RxList<FriendRequestModel> _sentRequests = <FriendRequestModel>[].obs;
+  final RxList<FriendRequestModel> _receivedRequests =
+      <FriendRequestModel>[].obs;
+
+  final RxList<FriendshipModel> _friendships = <FriendshipModel>[].obs;
+
+  List<UserModel> get users => _users;
+  List<UserModel> get filteredUsers => _filteredUsers;
+  bool get isLoading => _isLoading.value;
+  String get searchQuery => _searchQuery.value;
+  String get error => _error.value;
+  Map<String, UserRelationshipStatus> get userRelationships =>
+      _userRelationships;
+}
