3f4031a02d6a6554ea0d5de80949dde8e0b6ee01
Thisara Kavinda <thizarakavinda@gmail.com>
2026-04-30 22:21:34 +0530

    feat: add home tab to main view

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_getx_chat/controllers/main_controller.dart';
+import 'package:flutter_firebase_getx_chat/theme/app_theme.dart';
 import 'package:flutter_firebase_getx_chat/views/profile/profile_view.dart';
 import 'package:get/get.dart';
 
@@ -16,6 +17,30 @@ class MainView extends GetView<MainController> {
           Container(color: Colors.blue), // UsersListView(),
           ProfileView(), // ProfileView(),
         ],
+      ),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          currentIndex: controller.currentIndex,
+          onTap: controller.changeTabIndex,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: AppTheme.primaryColor,
+          unselectedItemColor: AppTheme.textSecondaryColor,
+          backgroundColor: Colors.white,
+          elevation: 8,
+          items: [
+            BottomNavigationBarItem(
+              icon: _buildIconWithBadge(
+                Icons.chat_outlined,
+                controller.getUnreadCount(),
+              ),
+              activeIcon: _buildIconWithBadge(
+                Icons.chat,
+                controller.getUnreadCount(),
+              ),
+              label: 'Chats',
+            ),
+          ],
+        ),
       ),
     );
   }
