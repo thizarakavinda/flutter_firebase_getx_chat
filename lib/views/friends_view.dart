afe20de19def8853d2a10c79001192959d4eb75b
Thisara Kavinda <thizarakavinda@gmail.com>
2026-05-03 15:22:42 +0530

    setup friends view screen

---
+++ lib/views/friends_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+
+import '../controllers/friends_controller.dart';
+
+class FriendsView extends GetView<FriendsController> {
+  const FriendsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
