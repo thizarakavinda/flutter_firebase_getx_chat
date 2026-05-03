bd99dd4a24b87f49c059919b9bde86904fff92a5
Thisara Kavinda <thizarakavinda@gmail.com>
2026-05-03 15:17:28 +0530

    feat: implement last seen show

--- lib/controllers/friends_controller.dart
+++ lib/controllers/friends_controller.dart
@@ -247,4 +247,25 @@ class FriendsController extends GetxController {
       _isLoading.value = false;
     }
   }
+
+  String getLastSeenText(UserModel user) {
+    if (user.isOnline) {
+      return 'Online';
+    } else {
+      final now = DateTime.now();
+      final difference = now.difference(user.lastSeen);
+
+      if (difference.inMinutes < 1) {
+        return 'Just now';
+      } else if (difference.inHours < 1) {
+        return 'Last seen ${difference.inMinutes} m ago';
+      } else if (difference.inDays < 1) {
+        return 'Last seen ${difference.inHours} h ago';
+      } else if (difference.inDays < 7) {
+        return 'Last seen ${difference.inDays} d ago';
+      } else {
+        return 'Last seen ${user.lastSeen.day}/${user.lastSeen.month}/${user.lastSeen.year}';
+      }
+    }
+  }
 }
