0e7661439735c941dd2ee470fdf49804e4e76321
Thisara Kavinda <thizarakavinda@gmail.com>
2026-05-03 14:19:58 +0530

    init variables in  friends controller

--- lib/controllers/friends_controller.dart
+++ lib/controllers/friends_controller.dart
@@ -1,3 +1,28 @@
+import 'dart:async';
+
+import 'package:flutter_firebase_getx_chat/controllers/auth_controller.dart';
+import 'package:flutter_firebase_getx_chat/models/friendship_model.dart';
+import 'package:flutter_firebase_getx_chat/models/user_model.dart';
+import 'package:flutter_firebase_getx_chat/services/firestore_service.dart';
 import 'package:get/get.dart';
 
-class FriendsController extends GetxController {}
+class FriendsController extends GetxController {
+  final FirestoreService _firestoreService = FirestoreService();
+  final AuthController _authController = Get.find<AuthController>();
+
+  final RxList<FriendshipModel> _friendships = <FriendshipModel>[].obs;
+  final RxList<UserModel> _friends = <UserModel>[].obs;
+  final RxBool _isLoading = false.obs;
+  final RxString _error = ''.obs;
+  final RxString _searchQuery = ''.obs;
+  final RxList<UserModel> _filteredFriends = <UserModel>[].obs;
+
+  StreamSubscription? _friendshipsSubscriptions;
+
+  List<FriendshipModel> get friendships => _friendships.toList();
+  List<UserModel> get friends => _friends;
+  List<UserModel> get filteredFriends => _filteredFriends;
+  bool get isLoading => _isLoading.value;
+  String get error => _error.value;
+  String get searchQuery => _searchQuery.value;
+}
