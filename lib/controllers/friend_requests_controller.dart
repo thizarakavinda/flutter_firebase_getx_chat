4d2d19fc7a3a3aab93f2f142c320a8e0fef79925
Thisara Kavinda <thizarakavinda@gmail.com>
2026-05-10 13:37:57 +0530

    init frind req cont elements

--- lib/controllers/friend_requests_controller.dart
+++ lib/controllers/friend_requests_controller.dart
@@ -1,3 +1,11 @@
+import 'package:flutter_firebase_getx_chat/controllers/auth_controller.dart';
+import 'package:flutter_firebase_getx_chat/models/friend_request_model.dart';
+import 'package:flutter_firebase_getx_chat/services/firestore_service.dart';
 import 'package:get/get.dart';
 
-class FriendRequestsController extends GetxController {}
+class FriendRequestsController extends GetxController {
+  final FirestoreService _firestoreService = FirestoreService();
+  final AuthController _authController = Get.find<AuthController>();
+  final RxList<FriendRequestModel> _receivedRequests = <FriendRequestModel>[].obs;
+  final RxList<FriendRequestModel> _sentRequests = <FriendRequestModel>[].obs;
+}
