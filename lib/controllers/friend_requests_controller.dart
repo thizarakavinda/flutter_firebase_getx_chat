80434c2c31cfda262de25d3f5dfcd8abb5b05522
Thisara Kavinda <thizarakavinda@gmail.com>
2026-05-10 14:58:06 +0530

    feat: get req time details

--- lib/controllers/friend_requests_controller.dart
+++ lib/controllers/friend_requests_controller.dart
@@ -107,4 +107,21 @@ class FriendRequestsController extends GetxController {
       _isLoading.value = false;
     }
   }
+
+  String getRequestTimeText(DateTime createdAt) {
+    final now = DateTime.now();
+    final difference = now.difference(createdAt);
+
+    if (difference.inMinutes < 1) {
+      return 'Just now';
+    } else if (difference.inHours < 1) {
+      return '${difference.inMinutes} m ago';
+    } else if (difference.inDays < 1) {
+      return '${difference.inHours} h ago';
+    } else if (difference.inDays < 7) {
+      return '${difference.inDays} d ago';
+    } else {
+      return '${createdAt.day}/${createdAt.month}/${createdAt.year}';
+    }
+  }
 }
