685cfa1e29adf28d38858a95839bd13f13bec4f4
Thisara Kavinda <thizarakavinda@gmail.com>
2026-05-13 15:25:16 +0530

    init home controlling setters

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,5 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_firebase_getx_chat/controllers/auth_controller.dart';
+import 'package:flutter_firebase_getx_chat/models/chat_model.dart';
+import 'package:flutter_firebase_getx_chat/models/user_model.dart';
 import 'package:get/get.dart';
 
+import '../models/notification_model.dart';
+
 class HomeController extends GetxController {
+
+  final FirebaseFirestore _firestoreService = FirebaseFirestore.instance;
+  final AuthController _authController = Get.find<AuthController>();
+
+  final RxList<ChatModel> _allChats = <ChatModel>[].obs;
+  final RxList<ChatModel> _filteredChats = <ChatModel>[].obs;
+  final RxList<NotificationModel> _notifications = <NotificationModel>[].obs;
+  final RxBool _isLoading = false.obs;
+  final RxString _error = ''.obs;
+  final RxMap<String, UserModel> _users = <String, UserModel>{}.obs;
+  final RxString _searchQuery = ''.obs;
+  final RxBool _isSearching = false.obs;
+  final RxString _activeFilter = 'All'.obs;
  
 }
