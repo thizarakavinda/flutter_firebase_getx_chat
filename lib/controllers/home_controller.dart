afb67e295e1e0d46d9b2ef3eae9a4f67b9eb992f
Thisara Kavinda <thizarakavinda@gmail.com>
2026-05-13 16:30:52 +0530

    feat: last message time format

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -88,4 +88,22 @@ class HomeController extends GetxController {
     }
     return null;
   }
+
+  String formatLastMessageTime(DateTime? time) {
+    if (time == null) return '';
+    final now = DateTime.now();
+    final difference = now.difference(time);
+
+    if (difference.inMinutes < 1) {
+      return 'Just now';
+    } else if (difference.inHours < 1) {
+      return '${difference.inMinutes}m ago';
+    } else if (difference.inDays < 1) {
+      return '${difference.inHours}h ago';
+    } else if (difference.inDays < 7) {
+      return '${difference.inDays}d ago';
+    } else {
+      return '${time.month}/${time.day}/${time.year}';
+    }
+  }
 }
